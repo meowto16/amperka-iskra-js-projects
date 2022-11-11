efb6e17f5a1e2692f74d6621b9a57b40a2e8a063
Maxim Kirshin <meowto16@gmail.com>
2022-11-11 09:29:02 +0500

    Project. 02. BPM Counter. Dealt With "Serial Projector"

--- 02. BPM Counter/index.js
+++ 02. BPM Counter/index.js
@@ -2,12 +2,16 @@ const led = require('@amperka/led').connect(P0);
 const buzzer = require('@amperka/buzzer').connect(P5)
 const button = require('@amperka/button').connect(P6)
 
+led.brightness(0.4)
+
 button.on('press', () => {
   buzzer.toggle(true);
   led.turnOn();
+  console.log(`Press`)
 });
 
 button.on('release', () => {
   buzzer.toggle(false);
   led.turnOff();
+  console.log(`Release`)
 });
