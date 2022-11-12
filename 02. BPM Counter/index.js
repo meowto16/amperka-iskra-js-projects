4c0a14dbc7f19330f00680c3262f99b3e8fabfb9
Maxim Kirshin <meowto16@gmail.com>
2022-11-12 14:32:12 +0500

    Project. 02. BPM Counter. Adds potentiometer

--- 02. BPM Counter/index.js
+++ 02. BPM Counter/index.js
@@ -1,17 +1,23 @@
-const led = require('@amperka/led').connect(P0);
+const led = require('@amperka/led').connect(P0)
 const buzzer = require('@amperka/buzzer').connect(P5)
 const button = require('@amperka/button').connect(P6)
+const regulator = require('@amperka/pot').connect(A0)
 
-led.brightness(0.4)
+const FRAME_RATE = 24
+const interval = 1000 / FRAME_RATE
 
 button.on('press', () => {
-  buzzer.toggle(true);
-  led.turnOn();
+  buzzer.toggle(true)
+  led.turnOn()
   console.log(`Press`)
 });
 
 button.on('release', () => {
-  buzzer.toggle(false);
-  led.turnOff();
+  buzzer.toggle(false)
+  led.turnOff()
   console.log(`Release`)
 });
+
+setInterval(() => {
+  led.brightness(1 - regulator.read());
+}, interval)
